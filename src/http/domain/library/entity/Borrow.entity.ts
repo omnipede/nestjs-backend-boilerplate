0946199e6f847343bfbd8c9563a9c75e6d6ff3ac
서현규 <omnipede@coinplug.com>
2020-11-25 16:14:52 +0900

    기능추가: entity builder 메소드 추가
    Column 의 null check 를 위해 builder 메소드를 추가함
    constructor 에 null check 로직을 넣어도 되지만, 이 경우 TypeOrm 에서 자동으로 entity 의 constructor 를 parameter 없이 호출하기 때문에
    constructor 의 parameter null check 로직에 걸리는 문제가 발생함.
    이를 피하기 위해 어쩔 수 없이 builder 메소드를 넣게 됨.

--- src/http/domain/library/entity/Borrow.entity.ts
+++ src/http/domain/library/entity/Borrow.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import expect from 'expect';
 import { Book } from "./Book.entity";
 import { User } from "./User.entity";
 
@@ -10,16 +11,42 @@ export class Borrow {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ManyToOne(() => User, user => user.borrowList, { onDelete: 'CASCADE' })
+  @ManyToOne(() => User, user => user.borrowList, { onDelete: 'CASCADE', nullable: false })
   @JoinColumn({ name: 'user_id' })
   user: User;
 
-  @ManyToOne(() => Book, book => book.borrowList, { onDelete: 'CASCADE' })
+  @ManyToOne(() => Book, book => book.borrowList, { onDelete: 'CASCADE', nullable: false })
   @JoinColumn({ name: 'book_id' })
   book: Book;
 
-  constructor(user: User, book: Book) {
-    this.user = user;
-    this.book = book;
+  @CreateDateColumn({ name: 'created_at'})
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  static Builder = class {
+    user: User;
+    book: Book;
+
+    public setUser(user: User) {
+      this.user = user;
+      return this;
+    }
+
+    public setBook(book: Book) {
+      this.book = book;
+      return this;
+    }
+
+    public build(): Borrow {
+      const { user, book } = this;
+      expect(user).not.toBeFalsy();
+      expect(book).not.toBeFalsy();
+      const borrow = new Borrow();
+      borrow.user = user;
+      borrow.book = book;
+      return borrow;
+    }
   }
 }
